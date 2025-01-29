9f9cc3937d3abd959c28c00216cf0eefba82c008
daniel-black <drblack651@gmail.com>
2025-01-28 17:53:00 -0700

    basic stuff

---
+++ components/full-width-image.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export function FullWidthImage({ src, alt }: { src: string; alt: string }) {
+  return (
+    <div className="relative w-full h-auto aspect-video">
+      <div className="absolute inset-0 flex flex-col justify-center p-6 md:p-12 z-10">
+        <h1 className="text-5xl text-white font-semibold text-center drop-shadow-lg">
+          We're getting married!
+        </h1>
+      </div>
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+          objectPosition: "center",
+        }}
+        priority
+        quality={100}
+        className="z-0 brightness-50"
+      />
+    </div>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header className="border-b flex items-center justify-between h-16 px-4 md:px-12">
+      <div>S+D</div>
+      <nav>nav link</nav>
+    </header>
+  );
+}
