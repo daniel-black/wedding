d8287a123fabc872c94776f230e5285fb8aa865e
daniel-black <drblack651@gmail.com>
2025-07-20 12:33:59 -0600

    more info in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,9 @@ import Header from "@/components/layout/header";
 import { displayFont, bodyFont, monoFont, handwrittenFont } from "@/lib/fonts";
 
 export const metadata: Metadata = {
-  title: "Wedding",
-  description: "Wedding",
+  title: "Dan & Steph's Wedding",
+  description:
+    "Daniel Black and Stephanie Castrillo are getting married on May 29th 2026 in Denver.",
 };
 
 export default function RootLayout({
