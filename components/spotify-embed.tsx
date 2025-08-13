46eb6e93fc9abb073c40925f9967c549ad670600
daniel-black <drblack651@gmail.com>
2025-08-13 09:20:25 -0600

    fix broken playlist link

--- components/spotify-embed.tsx
+++ components/spotify-embed.tsx
@@ -14,4 +14,4 @@ export function SpotifyEmbed() {
 }
 
 export const RECOMMENDATIONS_PLAYLIST_URL =
-  "https://open.spotify.com/playlist/1U75VzX2Mb4UA8tE9kqhWf?si=cb036cc290b44b00&pt=52b858b70e353f04ef211f095fb3418e";
+  "https://open.spotify.com/playlist/1U75VzX2Mb4UA8tE9kqhWf?si=Yeyi-fbzQUW5-h0CCHClnQ";
